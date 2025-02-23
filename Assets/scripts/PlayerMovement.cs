d2d08a62d3fd57eef80423e3ec8b216a6119e3ba
Akash Sarkar <sarkarakash1002@gmail.com>
2025-02-23 14:49:37 +0530

    Animations added and enemy functionality added

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -3,61 +3,28 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    //public float runSpeed = 40f; // Speed multiplier for horizontal movement
-    //private CharacterController2D controller;
-    //private float horizontalMove = 0f;
-    //private bool jump = false;
-    //private bool crouch = false;
-
-    //private void Awake()
-    //{
-    //    controller = GetComponent<CharacterController2D>();
-    //}
-
-    //void Update()
-    //{
-    //    // Get horizontal movement input (-1 to 1 for left/right movement)
-    //    horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-
-    //    // Check for jump input
-    //    if (Input.GetButtonDown("Jump"))
-    //    {
-    //        jump = true;
-    //    }
-
-    //    // Check for crouch input
-    //    if (Input.GetButtonDown("Crouch"))
-    //    {
-    //        crouch = true;
-    //    }
-    //    else if (Input.GetButtonUp("Crouch"))
-    //    {
-    //        crouch = false;
-    //    }
-    //}
-
-    //private void FixedUpdate()
-    //{
-    //    // Move the player using CharacterController2D's Move method
-    //    controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-    //    jump = false; // Reset jump after applying it
-    //}
-
     [SerializeField] CharacterController2D controller2D;
     float horinzontalValue;
     public float horinzontalSpeed;
     public bool jump;
+    [SerializeField] Animator m_animator;
 
     private void Awake()
     {
         controller2D = GetComponent<CharacterController2D>();
+        m_animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         horinzontalValue = Input.GetAxis("Horizontal");
+        m_animator.SetFloat("Speed",Mathf.Abs(horinzontalValue));
 
-        if (Input.GetKeyDown(KeyCode.W)) { jump = true; }
+        if (Input.GetKeyDown(KeyCode.W))
+        {           
+            jump = true; 
+            m_animator.SetBool("Jump",jump);
+        }
 
     }
 
@@ -67,6 +34,29 @@ public class PlayerMovement : MonoBehaviour
         {
             controller2D.Move(horinzontalValue * horinzontalSpeed * Time.fixedDeltaTime, false, jump);
             jump = false;
+        }
+    }
+
+    public void OnLand()
+    {
+        m_animator.SetBool("Jump",false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Hit "+ collision.gameObject.name);
+
+        if (collision.collider.tag == "EnemyHead")
+        {
+            //Kill the enemy
+            Debug.Log("Hit enemy in head");
+            Destroy(collision.gameObject);
+        }
+
+        else if(collision.collider.tag == "EnemyBody")
+        {
+            //Kill the player/ourself
+            gameObject.SetActive(false); Debug.Log("Hit enemy in body");
         }
     }
 }
