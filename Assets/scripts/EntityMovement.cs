89586729a83f4b017c62f46248da44565b1cd3ab
Akash Sarkar <sarkarakash1002@gmail.com>
2025-04-12 21:23:47 +0530

    Enemy movement modified

--- Assets/scripts/EntityMovement.cs
+++ Assets/scripts/EntityMovement.cs
@@ -13,13 +13,60 @@ public class EntityMovement : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        enabled = false;
     }
+
+    private void OnBecameVisible()
+    {
+#if UNITY_EDITOR
+        //enabled = !EditorApplication.isPaused;
+        enabled = true;
+#else
+        enabled = true;
+#endif
+    }
+
+    private void OnBecameInvisible()
+    {
+        enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        rb.WakeUp();
+    }
+
+    private void OnDisable()
+    {
+        rb.velocity = Vector2.zero;
+        rb.Sleep();
+    }
+
     private void FixedUpdate()
     {
         velocity.x = direction.x * speed;
         velocity.y += Physics2D.gravity.y * Time.fixedDeltaTime;
 
         rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+
+        if (rb.Raycast(direction))
+        {
+            direction = -direction;
+        }
+
+        if (rb.Raycast(Vector2.down))
+        {
+            velocity.y = Mathf.Max(velocity.y, 0f);
+        }
+
+        if (direction.x > 0f)
+        {
+            transform.localEulerAngles = new Vector3(0f, 180f, 0f);
+        }
+        else if (direction.x < 0f)
+        {
+            transform.localEulerAngles = Vector3.zero;
+        }
     }
 
 }
